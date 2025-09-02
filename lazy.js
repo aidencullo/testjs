820cd92f4865575fc8f6cc5d4f026b1f2aef6256
aidencullo <aidencullo@gmail.com>
2025-09-02 11:20:43 -0400

    Update lazy.js

--- lazy.js
+++ lazy.js
@@ -1,2 +1,6 @@
 document.body.textContent = ''
 document.write("this was written by js...")
+const div = document.createElement('div');
+div.textContent = "this is a div created by js!"
+document.body.appendChild(div);
+
